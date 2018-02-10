bf23713c7bfd7ca4c99f32bc73db128fef9116e7
Jebi <jebeli@bluewin.ch>
2018-02-10 20:53:01 +0100

    Copyright added to GDI files

--- GDITiles/Program.cs
+++ GDITiles/Program.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿/*
+Copyright © 2018 Jean Pascal Bellot
+
+This file is part of Tiles.
+
+Tiles is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Tiles is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Tiles.  If not, see http://www.gnu.org/licenses/
+*/
 
 namespace GDITiles
 {
+    using System;
+    using System.Windows.Forms;
+
     static class Program
     {
         /// <summary>
@@ -16,6 +30,7 @@ namespace GDITiles
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.DoEvents();
             Application.Run(new GDIForm());
         }
     }
