ab1b15ca0b0ee704367a2872fc8cda1fce7029ae
Jebi <jebeli@bluewin.ch>
2018-02-10 19:25:30 +0100

    Copyright added to Graphics

--- TileEngine/Graphics/AbstractGraphics.cs
+++ TileEngine/Graphics/AbstractGraphics.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TileEngine.Files;
+﻿/*
+Copyright © 2018 Jean Pascal Bellot
+
+This file is part of Tiles.
+
+Tiles is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Tiles is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Tiles.  If not, see http://www.gnu.org/licenses/
+*/
 
 namespace TileEngine.Graphics
 {
+    using System;
+    using Files;
+
     public abstract class AbstractGraphics : IGraphics, IDisposable
     {
         private long frameId;
